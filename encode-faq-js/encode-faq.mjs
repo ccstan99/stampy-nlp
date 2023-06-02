ffa47b4302c303f47e797db7ba6c5d1bc944adbb
Daniel O'Connell <github@ahiru.pl>
2023-06-02 22:36:59 +0200

    Add tensorflow alternative phrasings

--- encode-faq-js/encode-faq.mjs
+++ encode-faq-js/encode-faq.mjs
@@ -3,19 +3,9 @@
 // usage: node questions-encodings.mjs ${CODA_TOKEN}
 import * as tf from "@tensorflow/tfjs-node"; // do not remove import, `tf` is needed in following module even if not used in this file
 import * as use from "@tensorflow-models/universal-sentence-encoder";
-import fetch from "node-fetch";
 import * as fs from "fs";
+import fetch from "node-fetch";
 import { exit } from "process";
-
-const filepath = "./";
-const filename = filepath + "stampy-encodings.json";
-
-let prevNumQs = 0;
-if (fs.existsSync(filename)) {
-  const p = JSON.parse(fs.readFileSync(filename, "utf8"));
-  prevNumQs = p.numQs;
-  console.log(`${prevNumQs} questions from in previous generated encodings.`);
-}
 
 if (process.argv.length < 3) {
   console.log(
@@ -27,46 +17,120 @@ if (process.argv.length < 3) {
 
 const CODA_TOKEN = process.argv[2];
 const CODA_DOC_ID = "fau7sl2hmG";
-const table = "Answers";
-const params =
+let table = "table-1Q8_MjxUes"; // On site table
+let params =
   "useColumnNames=true&query=Status:%22Live%20on%20site%22&limit=1000";
-const url = `https://coda.io/apis/v1/docs/${CODA_DOC_ID}/tables/${table}/rows?${params}`;
+let url = `https://coda.io/apis/v1/docs/${CODA_DOC_ID}/tables/${table}/rows?${params}`;
 
-fetch(url, { headers: { Authorization: `Bearer ${CODA_TOKEN}` } })
-.then((response) => response.json())
-.then((r) => {
+const byScore = (a, b) => b.score - a.score;
+const normalize = (val) => val.toLowerCase();
 
-  const response = r.items;
-  console.log(`${response.length} questions fetched from ${url}.`);
+let getCodaRows = async (url) => {
+    const fetchItems = async (url) => {
+        const response = await fetch(url, { headers: { Authorization: `Bearer ${CODA_TOKEN}` } });
+        const data = await response.json();
+        return {
+            nextPage: data?.nextPageLink,
+            items: data.items.map(item => ({
+                name: item.name,
+                pageid: item.values["UI ID"],
+                alternates: item.values['Alternate Phrasings']
+            }))
+        };
+    };
 
+    let allItems = [];
+    let more = null;
+    do {
+        const {items, nextPage} = await fetchItems(url);
+        allItems = allItems.concat(items);
+        more = nextPage;
+    } while (Boolean(more))
+    return allItems;
+};
+
+let embedItems = async (model, items) => {
     const [questions, questionsNormalized, pageids] = [[], [], []];
-    response.forEach((item) => {
-        const names = item.values['All Phrasings'].split('\n').filter(Boolean);
-        names.forEach((name) => {
-            questions.push(name);
-            questionsNormalized.push(name.toLowerCase());
-            pageids.push(item.values["UI ID"]);
+    items.forEach((item, index) => {
+        questions.push(item.name);
+        questionsNormalized.push(normalize(item.name));
+        pageids.push(item.pageid);
+        item.index = index;
+    });
+
+    return { encodings: await model.embed(questionsNormalized), questions, questionsNormalized, pageids };
+};
+
+
+let compareQuestions = async (items, model) => {
+    const { encodings } = await embedItems(model, items);
+
+    return async (mainQuestionId, alternative, n=5) => {
+        const searchQuery = alternative.toLowerCase().trim().replace(/\s+/g, ' ');
+
+        // encodings is 2D tensor of 512-dims embeddings for each sentence
+        const top = await model.embed(searchQuery).then((encoding) => {
+            // numerator of cosine similar is dot prod since vectors normalized
+            const scores = tf.matMul(encoding, encodings, false, true).dataSync();
+
+            // tensorflow requires explicit memory management to avoid memory leaks
+            encoding.dispose();
+
+            const questionsScored = items.map(({ name, pageid, index }) => ({
+            name,
+                pageid: pageid,
+                score: scores[index],
+            }));
+            return questionsScored.sort(byScore).slice(0, n);
         });
-    });
+        return top.some(({ pageid }) => pageid == mainQuestionId);
+    };
+};
 
-  const numQs = questions.length;
+let getAlternativePhrasings = async (items, isDuplicate) =>
+    (await Promise.all(
+        items.filter(question => Boolean(question.alternates))
+            .map(({ pageid, name, alternates}) => alternates
+                 .split('\n').
+                 filter(q => q !== '' && q !== name)
+                 .map(async alternate => ({pageid, name, alternate, duplicate: await isDuplicate(pageid, alternate)})))
+            .flat()
+    )).filter(({duplicate}) => !duplicate);
 
-  use.load()
-  .then((model) => {
 
+let processAllRows = async () => {
+    const filepath = "./";
+    const filename = filepath + "stampy-encodings.json";
+
+    let prevNumQs = 0;
+    if (fs.existsSync(filename)) {
+        const p = JSON.parse(fs.readFileSync(filename, "utf8"));
+        prevNumQs = p.numQs;
+        console.log(`${prevNumQs} questions from in previous generated encodings.`);
+    }
+
+    let items = await getCodaRows(url);
+
+    console.log(`${items.length} questions fetched from ${url}.`);
+
+    const model = await use.load();
     console.log(`Tensorflow's universal sentence encoder model loaded.`);
-    model.embed(questionsNormalized)
-    .then((enc) => enc.arraySync())
-    .then((encodings) => {
 
-      console.log(`Questions encoded.`);
-      let data = JSON.stringify({ numQs, questions, pageids, encodings});
-      fs.writeFile(filename, data, (err) => {
+    let isDuplicate = await compareQuestions(items, model);
+    const alternativePhrasings = await getAlternativePhrasings(items, isDuplicate);
+    let allItems = items.concat(alternativePhrasings);
+
+    let { encodings, questions, pageids } = await embedItems(model, allItems);
+    let encoded = await encodings.arraySync();
+
+    console.log(`${allItems.length} questions encoded (${items.length} questions, ${alternativePhrasings.length} alternative phrasings).`);
+    let data = JSON.stringify({ numQs: questions.length, questions, pageids, encoded});
+    fs.writeFile(filename, data, (err) => {
         if (err) {
-          console.error(err);
+            console.error(err);
         }
-      });
-      console.log(`Successfully saved answered questions & encodings from Coda to ${filename}.`);
     });
-  });
-});
+    console.log(`Successfully saved answered questions & encodings from Coda to ${filename}.`);
+};
+
+await processAllRows();
