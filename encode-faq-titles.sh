7a6a94fde9425c0e5b8101e317ec9b652179302c
ccstan99 <ccstan99@gmail.com>
2023-06-04 03:32:47 -0700

    update url

--- encode-faq-titles.sh
+++ encode-faq-titles.sh
@@ -10,7 +10,7 @@ if [[ "$@" == *"-local"* ]]; then
     source venv/bin/activate
     python src/stampy_nlp/faq_titles.py $@
 else
-    NLP_API_URL=https://stampy-nlp-t6p37v2uia-uw.a.run.app/api/encode-faq-titles
+    NLP_API_URL=https://nlp.stampy.ai/api/encode-faq-titles
     curl -X POST -f -u stampy:$AUTH_PASSWORD $NLP_API_URL -H 'Content-Type: application/json' > stampy-duplicates.json
 fi
 
